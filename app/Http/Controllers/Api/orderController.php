595825ecba01332c0e81ae46e0902fd190a8ab53
Mohammad Abdul Baset <epbd.abir@gmail.com>
2023-02-18 18:28:38 +0600

    12

--- app/Http/Controllers/Api/orderController.php
+++ app/Http/Controllers/Api/orderController.php
@@ -45,7 +45,7 @@ class orderController extends Controller
             'userEmail' => 'required | string',
         ]);
         $cat = DB::table('exam_categories')->where('name', $data['categoryName'])->first();
-        $groups = User::where('email', $data['userEmail'])->join('exam_access', 'id', "=", 'exam_access.examinee')
+        $groups = User::where('email', $data['userEmail'])->join('exam_access', 'users.id', "=", 'exam_access.examinee')
             ->join('exam_groups', 'exam_groups.id', '!=', 'exam_access.exam_group_id')->where('exam_groups.exam_category_id', $cat->id)->whereNull('exam_access.exam_group_id')->get();
         if (!$cat) {
             return response([
