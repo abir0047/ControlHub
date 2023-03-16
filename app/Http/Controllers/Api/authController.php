088802c35867caf87279f39001693944c0711c58
Mohammad Abdul Baset <epbd.abir@gmail.com>
2023-03-16 17:51:43 +0600

    .

--- app/Http/Controllers/Api/authController.php
+++ app/Http/Controllers/Api/authController.php
@@ -243,7 +243,9 @@ class authController extends Controller
 				'name' => $request->displayName,
 				'email' => $request->email,
 				'password' => bcrypt($request->serverAuthCode),
-				'email_verified_at' => now(),
+			]);
+			User::where('id', $user->id)->update([
+				'email_verified_at' => date("Y-m-d H:i:s", time()),
 			]);
 
 			DB::table('exam_access')->insert([
